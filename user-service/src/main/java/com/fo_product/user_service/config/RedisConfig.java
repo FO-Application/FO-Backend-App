1982d9f19674368d74205479314fe67b8da09c63
UwUCode <dinhlol1012@gmail.com>
2025-11-19 16:05:48 +0700

    User caching fixed

--- user-service/src/main/java/com/fo_product/user_service/config/RedisConfig.java
+++ user-service/src/main/java/com/fo_product/user_service/config/RedisConfig.java
@@ -2,10 +2,12 @@ package com.fo_product.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -26,5 +28,15 @@ public class RedisConfig {
 
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
     }
 }
--- user-service/src/main/java/com/fo_product/user_service/resources/responses/UserResponse.java
+++ user-service/src/main/java/com/fo_product/user_service/resources/responses/UserResponse.java
@@ -2,16 +2,16 @@ package com.fo_product.user_service.resources.responses;
 
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Set;
 
 @Builder
-public record UserResponse(
+public record UserResponse (
         Long id,
         String email,
         String firstName,
         String lastName,
         String phone,
         LocalDate dob
-) {
+) implements Serializable {
 }
