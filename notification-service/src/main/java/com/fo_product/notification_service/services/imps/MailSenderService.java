757c797be49b49568c0c0e3ccbb8c61b65d702b1
UwUCode <dinhlol1012@gmail.com>
2025-12-03 10:27:56 +0700

    Latest commit

--- notification-service/src/main/java/com/fo_product/notification_service/services/imps/MailSenderService.java
+++ notification-service/src/main/java/com/fo_product/notification_service/services/imps/MailSenderService.java
@@ -9,6 +9,7 @@ import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -43,6 +44,9 @@ public class MailSenderService implements IMailSenderService {
 
             String htmlContent = templateEngine.process("otp-email", context);
             helper.setText(htmlContent, true);
+
+            ClassPathResource imageResource = new ClassPathResource("static/images/logo-fo-app.png");
+            helper.addInline("logoImage", imageResource);
 
             javaMailSender.send(message);
             log.info("Email send to: {}", to);
